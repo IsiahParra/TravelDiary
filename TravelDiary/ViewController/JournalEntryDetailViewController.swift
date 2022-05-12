b6e72f1003aa2805d6c40f182e037d4ee2d60edf
Isiah Parra <isiahparra@Isiahs-MacBook-Pro.local>
2022-05-12 12:12:53 -0600

    Finished Product

--- TravelDiary/ViewController/JournalEntryDetailViewController.swift
+++ TravelDiary/ViewController/JournalEntryDetailViewController.swift
@@ -16,14 +16,36 @@ class JournalEntryDetailViewController: UIViewController {
     //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
     }
     
     // MARK: Properties
+    var journalEntryReciever: JournalEntry?
     
     //MARK: Helper Functions
+    func updateViews () {
+        guard let journalEntry = journalEntryReciever else {return}
+        journalEntryTitleTextField.text = journalEntry.title 
+        journalEntrySummaryTextView.text = journalEntry.summary
+        journalEntryAddressTextField.text = journalEntry.address
+        jorunalEntryDateLabel.text = journalEntry.timestamp.stringValue()
+    }
     
     //MARK: Actions
     @IBAction func savedButtonTapped(_ sender: Any) {
+   guard let title = journalEntryTitleTextField.text,
+        let address = journalEntryAddressTextField.text,
+            let summary = journalEntrySummaryTextView.text else {return}
+        
+        
+        if let journalEntry = journalEntryReciever {
+            //if the receiver has a value they are trying to update the values
+            JournalEntryController.sharedInstance.update(entryToUpdate: journalEntry, newTitle: title, address: address, summary: summary)
+        } else {
+            //else - they are creating a new one
+            JournalEntryController.sharedInstance.createJournalEntry(withTitle: title, address: address, summary: summary)
+        }
+        navigationController?.popViewController(animated: true)
     }
 
    
--- TravelDiary/ViewController/Subviews/JournalEntryTableViewCell.swift
+++ TravelDiary/ViewController/Subviews/JournalEntryTableViewCell.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class JournalEntryTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var journalEntryNameLabel: UILabel!
+    @IBOutlet weak var journalEntryTitleLabel: UILabel!
     @IBOutlet weak var journalEntryAddressLabel: UILabel!
     @IBOutlet weak var journalEntryDateLabel: UILabel!
     
-    
-
+    func updateView(for journalEntry: JournalEntry) {
+        journalEntryTitleLabel.text = journalEntry.title
+        journalEntryAddressLabel.text = journalEntry.address
+        journalEntryDateLabel.text = journalEntry.timestamp.stringValue()
+    }
 }// End OF Class
